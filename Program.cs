c7f1f8e8bb104da0c6fdf7341e208c13d8c2d478
Jean Barbosa <jeanbarbosa.it@gmail.com>
2022-01-08 17:31:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppExecFixacaoInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
